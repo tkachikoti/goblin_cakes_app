f0a28fae08667a9c98e3ca56a0d519b0f16353b0
Tinashe Kachikoti <tinashekachikoti@gmail.com>
2022-06-03 19:04:44 +0100

    Add buttons to filter table by quarter

--- app/routes/sales.py
+++ app/routes/sales.py
@@ -18,26 +18,23 @@ def index():
     goblin_cake_sales = []
     sort_table_by = {
         'table_attribute': request.args.get('table_attribute', 'id'),
-        'order_by_descending': request.args.get('order_by_descending', '0')}
+        'order_by_descending': request.args.get('order_by_descending', '0'),
+        'quarter': request.args.get('quarter', '1')}
     sales_table_data = []
     table_attribute_list = []
 
     if bool(int(sort_table_by['order_by_descending'])):
-        goblin_cake_sales = db.session.query(GoblinCakeSales).order_by(
-            desc(sort_table_by['table_attribute'])).all()
+        goblin_cake_sales = db.session.query(GoblinCakeSales).filter(
+            GoblinCakeSales.quarter==sort_table_by['quarter']).order_by(
+                desc(sort_table_by['table_attribute'])).all()
     else:
-        goblin_cake_sales = db.session.query(GoblinCakeSales).order_by(
-            asc(sort_table_by['table_attribute'])).all()
+        goblin_cake_sales = db.session.query(GoblinCakeSales).filter(
+            GoblinCakeSales.quarter==sort_table_by['quarter']).order_by(
+                asc(sort_table_by['table_attribute'])).all()
 
     for element in goblin_cake_sales:
-        sales_table_data.append({
-            'id': element.id,
-            'product': element.product,
-            'product_type': element.product_type,
-            'price_per': element.price_per,
-            'units_sold': element.units_sold,
-            'quarter': element.quarter
-        })
+        del element.__dict__['_sa_instance_state']
+        sales_table_data.append(element.__dict__)
     
     if sales_table_data:
         for key in sales_table_data[0].keys():
